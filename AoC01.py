e80b2ab268de85cff5efc8c54c90df703693ffbc
trollp <32528463+trollp@users.noreply.github.com>
2022-12-01 12:07:06 +0100

    upload

---
+++ AoC01.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 10:28:24 2022 
+
+@author: ptroll 
+"""
+
+# read file by lines
+file1 = open('aoc22/aoc01.txt', 'r')
+Lines = file1.readlines()
+
+summary = 0
+list = []
+# Strips the newline character
+for line in Lines:
+    cal = str(line.strip())
+    if len(cal) == 0:
+        cal = 0
+   # print(int(cal))
+    cal = int(cal)
+    summary = summary + cal
+    if cal == 0:
+        #print(sum)
+        list.append(summary)
+        summary = 0
+list.sort()
+print(max(list))
+print(sum((list[-3:])))
