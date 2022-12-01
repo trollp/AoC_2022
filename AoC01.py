8431cc314d0fc6282cd6ab75841d39f751be2ac1
trollp <32528463+trollp@users.noreply.github.com>
2022-12-01 12:08:33 +0100

    Update AoC01.py

--- AoC01.py
+++ AoC01.py
@@ -10,7 +10,7 @@ Created on Thu Dec  1 10:28:24 2022
 file1 = open('aoc22/aoc01.txt', 'r')
 Lines = file1.readlines()
 
-summary = 0
+total = 0
 list = []
 # Strips the newline character
 for line in Lines:
@@ -18,10 +18,10 @@ for line in Lines:
     if len(cal) == 0:
         cal = 0
     cal = int(cal)
-    summary = summary + cal
+    total = total + cal
     if cal == 0:
-        list.append(summary)
-        summary = 0
+        list.append(total)
+        total = 0
 list.sort()
 print(max(list))
 print(sum((list[-3:])))
